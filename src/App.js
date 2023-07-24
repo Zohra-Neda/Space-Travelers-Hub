c691070e7e08cfba86b75b43e2ab70e0a25d70f9
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-24 22:12:09 +0630

    Add routeer for myprofile link

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Missions from './pages/Missions';
 import MyProfile from './pages/MyProfile';
+import Rockets from './pages/Rockets';
 
 const App = () => (
   <Routes>
+    <Route path="/" element={<Rockets />} />
     <Route path="/missions" element={<Missions />} />
     <Route path="/myprofile" element={<MyProfile />} />
   </Routes>
